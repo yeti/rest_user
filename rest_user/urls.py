af50d7b95bf34b07a56960106655f7f947379147
Baylee Feore <baylee@yetihq.com>
2014-09-11 22:53:02 -0700

    Sign up and login tests

--- rest_user/urls.py
+++ rest_user/urls.py
@@ -2,8 +2,8 @@ from django.conf.urls import patterns, url, include
 from rest_user.rest_user import views
 
 urlpatterns = patterns('',
-    url(r'^sign_up/$', views.SignUp.as_view()),
-    url(r'^login/$', views.Login.as_view()),
+    url(r'^sign_up/$', views.SignUp.as_view(), name="sign_up"),
+    url(r'^login/$', views.Login.as_view(), name="login"),
     url(r'^o/', include('oauth2_provider.urls', namespace='oauth2_provider')),
 )
 
