a80c29f1605aacd4cfa2144585f3b0feceda30c3
Baylee Feore <baylee@yeti.co>
2014-11-12 18:23:50 -0800

    Move following logic to be project-specific. Make UserSerializer more extensible

--- rest_user/serializers.py
+++ rest_user/serializers.py
@@ -1,5 +1,7 @@
+from django.conf import settings
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
+from manticore_django.manticore_django.utils import get_class
 from rest_core.rest_core.serializers import BaseModelSerializer
 
 __author__ = 'baylee'
@@ -30,10 +32,19 @@ class SignUpSerializer(LoginSerializer):
 
 
 class UserSerializer(BaseModelSerializer):
-    user_following_count = serializers.Field(source='user_following_count')
-    user_followers_count = serializers.Field(source='user_followers_count')
-
     class Meta:
         model = User
         exclude = ('password', 'last_login', 'is_active', 'is_admin', 'is_staff', 'is_superuser', 'groups',
                    'user_permissions')
+
+    def __init__(self, *args, **kwargs):
+        """
+        Due to issues with recursive importing, it's difficult to have our project-specific user serializer inherit
+        from this model. Instead, each project can define its own UserSerializer. Then we import those fields here
+        so that our other rest libraries will serialize the user model correctly.
+
+        TODO: allow graceful failure / default if a project-specific serializer isn't defined.
+        """
+        super(UserSerializer, self).__init__(*args, **kwargs)
+        custom_user_serializer = get_class(settings.USER_SERIALIZER)
+        self.fields = custom_user_serializer().fields
