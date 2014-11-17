655c8eca1948e631a5e297edb73dfcec55695eec
Baylee Feore <baylee@yeti.co>
2014-11-17 10:10:40 -0800

    Clean up unnecessary code

--- rest_user/views.py
+++ rest_user/views.py
@@ -1,12 +1,11 @@
 import base64
 from django.contrib.auth import get_user_model
-from django.core.exceptions import ValidationError
 from rest_framework import viewsets, generics, status
 from rest_framework.authentication import BasicAuthentication
 from rest_framework.decorators import list_route, detail_route
 from rest_framework.response import Response
 from rest_framework.exceptions import APIException
-from rest_core.rest_core.permissions import IsOwnerOrReadOnly, IsOwner
+from rest_core.rest_core.permissions import IsOwnerOrReadOnly
 from rest_user.rest_user.permissions import IsAuthenticatedOrCreate
 from rest_user.rest_user.serializers import SignUpSerializer, UserSerializer, LoginSerializer, PasswordSerializer
 
@@ -57,44 +56,3 @@ class UserViewSet(viewsets.ModelViewSet):
             return Response({'status': 'password set'})
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-class ChangePassword(generics.UpdateAPIView):
-    queryset = User.objects.all()
-    serializer_class = PasswordSerializer
-    permission_classes = (IsOwner,)
-
-    def update(self, request, *args, **kwargs):
-        partial = kwargs.pop('partial', False)
-        self.object = self.get_object_or_none()
-
-        # Once the serializer is called, the object becomes the updated version of the object. To access the original
-        # version of the user object to check password, we override the entire update method
-        if not request.DATA.get('old_password', None):
-            raise APIException("Must provide old password")
-        elif not request.DATA.get('password', None):
-            raise APIException("Must provide new password")
-        if not self.object.check_password(base64.decodestring(request.DATA['old_password'])):
-            raise APIException("Old password does not match")
-
-        serializer = self.get_serializer(self.object, data=request.DATA,
-                                         files=request.FILES, partial=partial)
-
-        if not serializer.is_valid():
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-        try:
-            self.pre_save(serializer.object)
-        except ValidationError as err:
-            # full_clean on model instance may be called in pre_save,
-            # so we have to handle eventual errors.
-            return Response(err.message_dict, status=status.HTTP_400_BAD_REQUEST)
-
-        if self.object is None:
-            self.object = serializer.save(force_insert=True)
-            self.post_save(self.object, created=True)
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-
-        self.object = serializer.save(force_update=True)
-        self.post_save(self.object, created=False)
-        return Response(serializer.data, status=status.HTTP_200_OK)
